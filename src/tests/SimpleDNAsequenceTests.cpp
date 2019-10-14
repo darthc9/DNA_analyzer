67b10b14a845ff2a2f799022ddf80281eb3615d5
Fadi A <fadi.abboud@gmail.com>
2019-10-14 11:34:34 +0300

    moved simple_dna_sequence tests to separate file

---
+++ src/tests/SimpleDNAsequenceTests.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+
+// test function to check for legal nucleotide character
+// this is not TDD - you already have this function!!!!!
+#include "simple-dna-sequence/nucleotide.h"
+TEST(Simple_DNA_aequence, Nucleotide_isLegalLiteral){
+EXPECT_TRUE(Nucleotide::isLegalLiteral('a'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('A'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('c'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('C'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('g'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('G'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('t'));
+EXPECT_TRUE(Nucleotide::isLegalLiteral('T'));
+
+EXPECT_FALSE(Nucleotide::isLegalLiteral('x'));
+EXPECT_FALSE(Nucleotide::isLegalLiteral('Y'));
+EXPECT_FALSE(Nucleotide::isLegalLiteral('z'));
+}
